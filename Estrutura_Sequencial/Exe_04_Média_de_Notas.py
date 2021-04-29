d5c24e2409ccfdea621bff2e976b4a30194d0fa3
Webster-TI <ti.spacacheri@gmail.com>
2021-04-29 12:05:13 -0300

    Criado programa média de notas.

---
+++ Estrutura_Sequencial/Exe_04_Média_de_Notas.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+nota_1 = int(input('Insira o valor da primeira nota: '))
+nota_2 = int(input('Insira o valor da primeira nota: '))
+nota_3 = int(input('Insira o valor da primeira nota: '))
+nota_4 = int(input('Insira o valor da primeira nota: '))
+resultado = nota_1 + nota_2 + nota_3 + nota_4
+media = resultado / 4
+print(f'A média das notas informadas é {media}')

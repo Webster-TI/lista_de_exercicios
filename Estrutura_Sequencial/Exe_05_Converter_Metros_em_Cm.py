047ec3f65b6001cbb85e98b9daf20125d77b6b5c
Webster-TI <ti.spacacheri@gmail.com>
2021-04-29 12:08:27 -0300

    Criado programa converter metros em centímetros.

---
+++ Estrutura_Sequencial/Exe_05_Converter_Metros_em_Cm.py
@@ -0,0 +1,4 @@
+#Faça um programa que converta metros para centímetros.
+metros = int(input('Insira um valor em metros: ', ))
+centimetros = metros * 100
+print(metros, 'metros é equivalente há', centimetros, 'centímetros')

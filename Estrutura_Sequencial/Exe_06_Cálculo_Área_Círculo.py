cd9af341cda1005ba5463e9d418206ef3e6af00d
Webster-TI <ti.spacacheri@gmail.com>
2021-04-30 09:44:25 -0300

    Criado programa para calcular a área de um circulo.

---
+++ Estrutura_Sequencial/Exe_06_Cálculo_Área_Círculo.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+#A = π·r²
+raio = float(input('Insira o valor do raio do círculo: ',))
+pi = 3.14
+a = (pi * raio) ** 2
+print(f'O círculo tem {a} cm de área')
+

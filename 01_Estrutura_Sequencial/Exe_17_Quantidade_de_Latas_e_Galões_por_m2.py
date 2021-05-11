ac5266af5f467ceef95f2f246947ce1dcb481852
Webster Spacacheri <ti.spacacheri@gmail.com>
2021-05-11 11:20:43 -0300

    Melhorado o código para cálculo de tintas.

--- 01_Estrutura_Sequencial/Exe_17_Quantidade_de_Latas_e_Galões_por_m2.py
+++ 01_Estrutura_Sequencial/Exe_17_Quantidade_de_Latas_e_Galões_por_m2.py
@@ -7,35 +7,60 @@
 #misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os
 # valores para cima, isto é, considere latas cheias.
 import math
-from typing import Union
 
-m2_por_litro = 6
-preco_lata = 80.00
-preco_galao = 25.00
-litros_lata = 18
-litros_galao = 3.6
-margem_seguranca = 1.1
+m2 = float(input('Insira a área em m² a ser pintada: '))
+margem_segurança = 1.1
+area_total = m2 * margem_segurança
+litros_por_m2 = 6
+litros_por_lata = 18
+litros_por_galao = 3.6
+valor_lata = 80
+valor_galao = 25
+litros_usados = (m2 * margem_segurança) / litros_por_m2
+litros_restantes = litros_usados % litros_por_lata
 
-m2 = float(input('Informe a quantidade de metros quadrados (m²) a serem pintados: '))
+qtd_apenas_latas = math.ceil(litros_usados / litros_por_lata)
+valor_apenas_latas = qtd_apenas_latas * valor_lata
+print(f'Você deverá usar {qtd_apenas_latas} latas de 18 litros, no valor de R$ {valor_apenas_latas}')
 
-consumo_litro = m2 / m2_por_litro * margem_seguranca
+qtd_apenas_galoes = math.ceil(litros_usados / litros_por_galao)
+valor_apenas_galoes = qtd_apenas_galoes * valor_galao
+print(f'Você deverá usar {qtd_apenas_galoes} galões de 3.6 litros, no valor de R$ {valor_apenas_galoes}')
 
-qtd_lata = math.ceil(consumo_litro / litros_lata)
-valor_lata = qtd_lata * preco_lata
+qtd_latas = math.floor(litros_usados / litros_por_lata)
+preco_lata = qtd_latas * valor_lata
 
-qtd_galao = math.ceil(consumo_litro / litros_galao)
-valor_galao = qtd_galao * preco_galao
+qtd_galoes = math.ceil(litros_restantes / litros_por_galao)
+preco_galao = qtd_galoes * valor_galao
 
-qtd_galao_misto = consumo_litro // litros_galao
-qtd_lata_misto = math.ceil((consumo_litro - qtd_galao_misto * litros_galao / litros_lata))
+preco_total = preco_lata + preco_galao
 
-valor_lata_misto = qtd_lata_misto * preco_lata
-valor_galao_misto = qtd_galao_misto * preco_galao
+print(f'Você deverá usar {qtd_latas} de 18 litros mais {qtd_galoes} galões de 3,6 litros, no valor de R$ {preco_total}')
 
-print(f'A quantidade de latas a serem usadas é {qtd_lata:.0f}, com valor total de R$ {valor_lata}')
-print(f'A quantidade de galões a serem usados é {qtd_galao:.0f}, com valor total de R$ {valor_galao}')
-print(f'Evitando menor desperdício de tinta temos:')
-print(f'Quantidade de latas: {qtd_lata_misto}')
-print(f'Quantidade de galões: {qtd_galao_misto}')
-print(f'A soma de latas e galões é de : {qtd_lata_misto + qtd_galao_misto}')
-print(f'O valor total da soma de latas e galões é de R$ {valor_lata_misto + valor_galao_misto}')
+
+
+#area_a_ser_pintada = float(input('Insira a área em m² a ser pintada: '))
+
+#area_com_folga = area_a_ser_pintada * 1.1
+#litros_por_metro = 6
+#qtd_litros_usados = area_com_folga / litros_por_metro
+
+#litros_por_lata = 18
+#qtd_latas = math.ceil(qtd_litros_usados / litros_por_lata)
+#valor_apenas_latas = qtd_latas * 80
+
+#litros_por_galao = 3.6
+#qtd_galoes = math.ceil(qtd_litros_usados / litros_por_galao)
+#valor_apenas_galoes = qtd_galoes * 25
+
+#qtd_de_latas = math.floor(qtd_litros_usados / litros_por_lata)
+#valor_latas = qtd_de_latas * 80
+#litros_restantes = qtd_litros_usados % litros_por_lata
+#qtd_de_galoes = math.ceil(litros_restantes / litros_por_galao)
+#valor_galoes = qtd_de_galoes * 25
+
+#valor_total = valor_latas + valor_galoes
+
+#print(f'Você deverá usar {qtd_latas} latas de 18 litros, no valor de R$ {valor_apenas_latas}')
+#print(f'Você deverá usar {qtd_galoes} galões de 3,6 litros, no valor de R$ {valor_apenas_galoes}')
+#print(f'Você deverá usar {qtd_de_latas} de 18 litros mais {qtd_de_galoes} galões de 3,6 litros, no valor de R$ {valor_total}')

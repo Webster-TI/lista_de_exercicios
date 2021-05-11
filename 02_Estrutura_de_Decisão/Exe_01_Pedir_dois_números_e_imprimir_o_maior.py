f8c14a2370f90001d712559844968b2f4275bfa5
Webster Spacacheri <ti.spacacheri@gmail.com>
2021-05-11 11:28:58 -0300

    Criado o pprograma pedindo dois números e imprimindo o maior deles.

---
+++ 02_Estrutura_de_Decisão/Exe_01_Pedir_dois_números_e_imprimir_o_maior.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+numero_1 = input('Insira o primeiro número: ')
+numero_2 = input('Insira o segundo número: ')
+if numero_1 > numero_2:
+        print(numero_1)
+else:
+        print(numero_2)

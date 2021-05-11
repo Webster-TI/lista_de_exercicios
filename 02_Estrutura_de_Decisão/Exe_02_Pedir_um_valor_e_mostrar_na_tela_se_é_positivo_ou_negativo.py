c13f15358692ac63eb18cf923f5cd1cbd258b105
Webster Spacacheri <ti.spacacheri@gmail.com>
2021-05-11 11:36:26 -0300

    Criado o pprograma pedindo um valor e mostrando se é POSITIVO ou NEGATIVO.

---
+++ 02_Estrutura_de_Decisão/Exe_02_Pedir_um_valor_e_mostrar_na_tela_se_é_positivo_ou_negativo.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+valor = int(input('Insira um número: '))
+if valor > 0:
+    print('O número inserido é POSITIVO')
+else:
+    print('O número inserido é NEGATIVO')
